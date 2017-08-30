7555dda9ca35fc7f5e1ca8d26f932c2852ae5917
spartrekus <spartrekus@gmail.com>
2017-08-30 13:10:38 +0200

    Update debootstrap.sh

--- debootstrap.sh
+++ debootstrap.sh
@@ -5,5 +5,7 @@
 
   debootstrap --no-check-gpg     --include=wpasupplicant  experimental   /target http://ftp.debian.org/debian/  ; mkdir /target/lib/modules
 
+# you need to upgrade debootstrap (make install) and to use testing (instead of experimental)
+# experimental is rc buggy 
 
 
